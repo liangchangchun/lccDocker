5754d8cf5a6bda78da4010ddd5472070c4c0ee85
liangchangchun <liangcc@lovego.com>
2018-04-13 19:05:31 +0800

    zookeeper集群搭建脚本

---
+++ debianjdk8/build.sh
@@ -0,0 +1,1 @@
+docker build -t="liangchangchun/debianjdk8:v2" .
---
+++ debianzookeeper/bak2/run_root.sh
@@ -0,0 +1,3 @@
+docker run  --privileged=true -itd -p 2181:2181 -v /data/lccDocker/debianzookeeper/data:/home/zookeeper/data -v /data/lccDocker/debianzookeeper/conf:/usr/local/zookeeper/conf -v /data/lccDocker/debianzookeeper/logs:/usr/local/zookeeper/logs -v /data/lccDocker/debianzookeeper/datalogs:/home/zookeeper/datalogs --name zookeeper_root liangchangchun/debianzookeeper:v2
+# Zookeeper的ZOO_LOG_DIR环境变量可以设置Zookeeper的系统日志输出到文件路径，但是配置了ZOO_LOG_DIR环境变量对应的目录也不会有日志文件，还需要配置环境变量ZOO_LOG4J_PROP=INFO,ROLLINGFILE，并且修改log4j.properties的zookeeper.root.logger=INFO, ROLLINGFILE，默认的日志是输出到CONSOLE的。
+# http://yangyoupeng-cn-fujitsu-com.iteye.com/blog/1922459
